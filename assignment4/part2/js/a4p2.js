c0e0d45fc8764c4d43715a51f9b3aba684f1d26f
AmbarJCR <ambarcerino27@gmail.com>
2026-04-02 12:45:01 -0400

    comments

---
+++ assignment4/part2/js/a4p2.js
@@ -0,0 +1,12 @@
+/* 
+    Name: Ambar Cerino
+    File: a4p2.js
+    Date: 02 April, 2026
+    Description: assignment 4 part 2 javascript 
+*/
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
