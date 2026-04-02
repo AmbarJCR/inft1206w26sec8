a6383ff906b8cea74e6afd96392a1d370631f798
AmbarJCR <ambarcerino27@gmail.com>
2026-04-02 13:32:21 -0400

    Add the images to the thumbnail bar

--- assignment4/part2/js/a4p2.js
+++ assignment4/part2/js/a4p2.js
@@ -14,10 +14,25 @@ const overlay = document.querySelector(".overlay");
 
 // Create a data object - array of objects called images
 let images = [
-    {filename: "pic1.jpg", alt: "Closeup of a human eye"},
-    {filename: "pic2.jpg", alt: "Rock that looks like a wave"},
-    {filename: "pic3.jpg", alt: "Purple and white pansies"},
-    {filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
-    {filename: "pic5.jpg", alt: "Large moth on a leaf"}
+    { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+    { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+    { filename: "pic3.jpg", alt: "Purple and white pansies" },
+    { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+    { filename: "pic5.jpg", alt: "Large moth on a leaf" }
 ];
 
+// Add the images to the thumbnail bar
+const baseURL = "./img";
+for (const image of images) {
+    const newImg = document.createElement("img");
+    newImg.src = baseURL +images;
+    newImg.alt = image.alt;
+    newImg.tabIndex = "0";
+    thumbBar.appendChild(newImg)
+    newImg.addEventListener("click", updateDisplayedImage);
+    newImg.addEventListener("keydown", (upDisplayedImg) => {
+        if (upDisplayedImg.code === "Enter") {
+            updateDisplayedImage(upDisplayedImg);
+        }
+    });
+};
