ce392d33a4995024058af4702025ad0c680e0ab3
AmbarJCR <ambarcerino27@gmail.com>
2026-04-03 18:07:57 -0400

    defining EvilCircle

--- assignment4/part4/js/a4p4.js
+++ assignment4/part4/js/a4p4.js
@@ -34,7 +34,7 @@ function randomRGB() {
 //Modeling a ball
 class Ball extends Shape {
     constructor(x, y, velX, velY, color, size) {
-        super (x, y, velX, velY);
+        super(x, y, velX, velY);
         this.color = color;
         this.size = size;
         this.exists = true;
@@ -68,45 +68,72 @@ class Ball extends Shape {
 
     // Raplacing collision detection 
     collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball) && ball.exists) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
+        for (const ball of balls) {
+            if (!(this === ball) && ball.exists) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
+                if (distance < this.size + ball.size) {
+                    ball.color = this.color = randomRGB();
+                }
+            }
         }
-      }
     }
-  }
 }
 
 const balls = [];
 
-    // Animating the ball
-    while(balls.length < 25) {
-        const size = random(10, 20);
-        const ball = new Ball(
-            random(0 + size, height - size),
-            random(0 + size, width - size),
-            random(-7, 7),
-            random(-7, 7),
-            randomRGB(),
-            size,
-        );
-        balls.push(ball);
-    }
+// Animating the ball
+while (balls.length < 25) {
+    const size = random(10, 20);
+    const ball = new Ball(
+        random(0 + size, height - size),
+        random(0 + size, width - size),
+        random(-7, 7),
+        random(-7, 7),
+        randomRGB(),
+        size,
+    );
+    balls.push(ball);
+}
 
-    function loop() {
-        ctx.fillStyle = "rgb(0 0 0 / 25%)";
-        ctx.fillRect(0, 0, width, height);
-        for (const ball of balls) {
-            ball.draw();
-            ball.update();
-            ball.collisionDetect();
-        }
-        requestAnimationFrame(loop);
+function loop() {
+    ctx.fillStyle = "rgb(0 0 0 / 25%)";
+    ctx.fillRect(0, 0, width, height);
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+        ball.collisionDetect();
     }
-    
+    requestAnimationFrame(loop);
+}
+
 loop();
+
+// Defining EvilCircle
+class EvilCircle extends Shape {
+    // EvilCircle constructor
+    constructor(x, y) {
+        super(x, y, 20, 20);
+        this.color = white;
+        this.size = 10;
+
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "a":
+                    this.x -= this.velX;
+                    break;
+                case "d":
+                    this.x += this.velX;
+                    break;
+                case "w":
+                    this.y -= this.velY;
+                    break;
+                case "s":
+                    this.y += this.velY;
+                    break;
+            }
+        });
+    }
+}
