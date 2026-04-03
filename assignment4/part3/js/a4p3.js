47976b3387af2ea76a1159145e0a722a4ef15981
AmbarJCR <ambarcerino27@gmail.com>
2026-04-03 00:29:19 -0400

    comments

--- assignment4/part3/js/a4p3.js
+++ assignment4/part3/js/a4p3.js
@@ -1,3 +1,10 @@
+/* 
+    Name: Ambar Cerino
+    File: a4p3.js
+    Date: 03 April, 2026
+    Description: assignment 4 part 3 javascript 
+*/
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
