b74890f229f86f8f5ebec323e6abf2073c56b7d0
AmbarJCR <ambarcerino27@gmail.com>
2026-04-03 00:34:40 -0400

    modeling a ball

--- assignment4/part3/js/a4p3.js
+++ assignment4/part3/js/a4p3.js
@@ -10,3 +10,24 @@ const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
+
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+}
+
+//Modeling a ball
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+}
